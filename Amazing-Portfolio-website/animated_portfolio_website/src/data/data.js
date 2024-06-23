3eec4d2cebe81ff3468f7d084188179647f6fc48
Web Addicted <155007617+WebAddicted9@users.noreply.github.com>
2024-06-23 08:02:58 +0530

    Add files via upload

---
+++ Amazing-Portfolio-website/animated_portfolio_website/src/data/data.js
@@ -0,0 +1,25 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNnKb5dCzEQl5jiA9BCYSJxZO0n4p70_TOXA&s",
+    title: "Ecommerce Website",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRMH9-0hxzhTvWtbMcgqGo8_rkpOurt_BTdg&s",
+    title: "Weather Forecasting Api",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLaUaU78h6399gBjo-Rb9YAmA3C-bf8THElA&s",
+    title: "A Currency Converter",
+    desc: "",
+    link: "",
+    git: "",
+  },
+];
